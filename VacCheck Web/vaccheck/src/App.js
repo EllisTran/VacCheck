53b2d76f1a53c161e9854d049288ece0f8647df2
SungWooKim-cs <72055468+SungWooKim-cs@users.noreply.github.com>
2021-10-18 15:05:58 -0500

    Feature: Created Web Auth

--- VacCheck Web/vaccheck/src/App.js
+++ VacCheck Web/vaccheck/src/App.js
@@ -1,7 +1,105 @@
-import SignupPage from "./components/SignupPage/SignupPage";
+// import SignupPage from "./components/SignupPage/SignupPage";
 
-function App() {
-  return <SignupPage></SignupPage>;
-}
+// function App() {
+//   return <SignupPage></SignupPage>;
+// }
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import fire from "./fire";
+import Login from "./components/Login";
+
+const App = () => {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(""); //for error-case(email)
+  const [passwordError, setPasswordError] = useState(""); //for error-case(password)
+  const [hasAccount, setHasAccount] = useState(false); //for switch in signin - login
+
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        // catching errors
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleSignup = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        // catching errors
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleLogOut = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  return (
+    <div className="app">
+      <Login //props for Login.
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleLogin={handleLogin}
+        handleSignup={handleSignup}
+        hasAccount={hasAccount}
+        setHasAccount={setHasAccount}
+        emailError={emailError}
+        passwordError={passwordError}
+      />
+    </div>
+  );
+};
 
 export default App;
