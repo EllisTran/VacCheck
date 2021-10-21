c606bc719d85ffdb5aace85c26633bde9c66f686
SungWooKim-cs <72055468+SungWooKim-cs@users.noreply.github.com>
2021-10-21 14:30:14 -0500

    Feature/Router

--- VacCheck Web/vaccheck/src/App.js
+++ VacCheck Web/vaccheck/src/App.js
@@ -1,13 +1,16 @@
-
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import {auth} from "../src/firebase";
+import { auth } from "../src/firebase";
 import Login from "./components/Login";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
-// import { useHistory } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  useHistory,
+} from "react-router-dom";
 import SignupPage from "../src/components/SignupPage/SignupPage";
 
 const App = () => {
+  const history = useHistory();
   const [user, setUser] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -27,31 +30,31 @@ const App = () => {
 
   const handleLogin = () => {
     clearErrors();
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .catch((err) => {
-        // console.log(fire);
-        // catching errors
-        switch (err.code) {
-          case "auth/invalid-email":
-          case "auth/user-disabled":
-          case "auth/user-not-found":
-            setEmailError(err.message);
-            break;
-          case "auth/wrong-password":
-            setPasswordError(err.message);
-            break;
-        }
-      });
+    auth.signInWithEmailAndPassword(email, password).catch((err) => {
+      // catching errors
+      switch (err.code) {
+        case "auth/invalid-email":
+        case "auth/user-disabled":
+        case "auth/user-not-found":
+          setEmailError(err.message);
+          break;
+        case "auth/wrong-password":
+          setPasswordError(err.message);
+          break;
+      }
+    });
   };
 
   const handleSignup = () => {
+    history.push("/SignupPage");
     clearErrors();
     auth
-      .createUserWithEmailAndPassword(email, password).then(data => {
+      .createUserWithEmailAndPassword(email, password)
+      .then((data) => {
         console.log("User ID :- ", data.user.uid);
-      })  
+      })
       .catch((err) => {
+        // catching errors
         switch (err.code) {
           case "auth/email-already-in-use":
           case "auth/invalid-email":
@@ -85,25 +88,23 @@ const App = () => {
 
   return (
     <div className="app">
-      {/* <Router>
-      <Switch>
-        <Route exact path="/SignupPage">
+      <switch>
+        <Route exact path="/" component={Login}>
+          <Login //props for Login.
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            handleLogin={handleLogin}
+            handleSignup={handleSignup}
+            hasAccount={hasAccount}
+            setHasAccount={setHasAccount}
+            emailError={emailError}
+            passwordError={passwordError}
+          />
         </Route>
-      </Switch>
-    </Router> */}
-      <Login //props for Login.
-        email={email}
-        setEmail={setEmail}
-        password={password}
-        setPassword={setPassword}
-        handleLogin={handleLogin}
-        handleSignup={handleSignup}
-        hasAccount={hasAccount}
-        setHasAccount={setHasAccount}
-        emailError={emailError}
-        passwordError={passwordError}
-      />
-      <SignupPage></SignupPage>
+        <Route exact path="/SignupPage" component={SignupPage} />
+      </switch>
     </div>
   );
 };
--- VacCheck Web/vaccheck/src/components/SignupPage/SignupPage.js
+++ VacCheck Web/vaccheck/src/components/SignupPage/SignupPage.js
@@ -3,10 +3,7 @@ import AccountFilter from "./AccountFilter";
 import BusinessForm from "./BusinessForm";
 import HealthProfessionalForm from "./HealthProfessionalForm";
 import PersonalUserForm from "./PersonalUserForm";
-
 import { db } from "../../firebase";
-
-
 
 const SignupPage = () => {
   const [accountType, setAccountType] = useState("");
@@ -23,9 +20,8 @@ const SignupPage = () => {
 
   // Replace her docId with authID
   const createDoc = async (newAccountInfo) => {
-    console.log(newAccountInfo);
     // Give me Auth id here... replace db.collection("users").doc().set.... with db.collection("users").doc(auth_id).set....
-    db.collection("users").doc("LA").set({
+    db.collection("users").doc("LA").set({ //instead of LA put auth_id 
       ...newAccountInfo,
       // put userId: auth_id here
     });
