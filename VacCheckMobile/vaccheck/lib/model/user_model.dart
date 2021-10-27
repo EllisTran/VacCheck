127944325626ea9c1b1cfa62d0f33f73421d0d53
Ellis Tran <ellisktran@gmail.com>
2021-10-27 18:35:52 -0500

    Can log in as a business user and login to normal user with additional information displaying

--- VacCheckMobile/vaccheck/lib/model/user_model.dart
+++ VacCheckMobile/vaccheck/lib/model/user_model.dart
@@ -5,22 +5,25 @@ class UserModel {
   String? userId;
   Map<String, dynamic>? userType;
   int? numVac;
-  UserModel(this.name, this.dateOfBirth, this.email,
-      [userId, userType, numVac]);
+  UserModel(this.name, this.email, [dateOfBirth, userId, userType, numVac]);
   String generateUniqueCode() {
     String genCode = "${numVac}${name}+${userId}${DateTime.now()}";
     return genCode;
   }
 
-  factory UserModel.fromMap(dynamic data) {
-    print(data['numVac']);
-    UserModel user = UserModel(
-        data['name'] ?? '',
-        data['dateOfBirth']?.toDate() ?? DateTime(2000, 1, 1),
-        data['email'] ?? '');
+  factory UserModel.UserMap(dynamic data) {
+    UserModel user = UserModel(data['name'] ?? '', data['email'] ?? '');
+    user.dateOfBirth = data['dateOfBirth']?.toDate() ?? DateTime(2000, 1, 1);
     user.numVac = data['numVac'] ?? 0;
     user.userId = data['userId'] ?? '';
     user.userType = data['userType'] ?? {};
     return user;
+  }
+
+  factory UserModel.BusinessMap(dynamic data) {
+    UserModel business = UserModel(data['name'] ?? '', data['email'] ?? '');
+    business.userId = data['userId'] ?? '';
+    business.userType = data['userType'] ?? {};
+    return business;
   }
 }
--- VacCheckMobile/vaccheck/lib/views/auth_views/signup_view.dart
+++ VacCheckMobile/vaccheck/lib/views/auth_views/signup_view.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:provider/src/provider.dart';
-import 'package:vaccheck/views/main_page_view.dart';
 import 'package:vaccheck/controller/auth_controller.dart';
 import 'package:vaccheck/firebase/auth_service.dart';
 import 'package:vaccheck/model/user_model.dart';
@@ -123,7 +122,7 @@ class _SignUpViewState extends State<SignUpView> {
                           password != null &&
                           name != null &&
                           dateOfBirth != null) {
-                        UserModel newUser = UserModel(name!, dateOfBirth!, email!);
+                        UserModel newUser = UserModel(name!, email!, dateOfBirth!);
                         try {
                           context
                               .read<AuthService>()
